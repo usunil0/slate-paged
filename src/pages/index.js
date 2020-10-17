f06efdad7358ef1a6ff699c577d022c07c9c1b69
usunil0 <usunil0@gmail.com>
2020-10-17 20:09:26 +0530

    move notes to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Notes from '../notes'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-         <div>
-      <Notes />
-    </div>
+      <div>
+        <Notes />
+      </div>
     </div>
   )
 }
